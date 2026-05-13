fb49deda616b7ee911664383c0e40f02f5203799
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-05-13 15:00:05 -0300

    Debug option

--- src/main.py
+++ src/main.py
@@ -3,11 +3,18 @@ from slowapi.errors import RateLimitExceeded
 
 from .exceptions.rate_limit import rate_limit_exceeded_handler
 from .routers import group_router
+from .configs.environment import DEBUG
+
 
 app = FastAPI(
-    # docs_url=None,
-    # redoc_url=None
+    docs_url=None,
+    redoc_url=None,
+    debug=False
 )
+if DEBUG:
+    app = FastAPI(
+        debug=True
+    )
 app.add_exception_handler(
     exc_class_or_status_code=RateLimitExceeded, handler=rate_limit_exceeded_handler
 )
