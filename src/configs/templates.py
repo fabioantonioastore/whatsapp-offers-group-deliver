ec371be4d3263f852670a0606771d11c0ddc1c20
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-05-19 16:51:25 -0300

    fix

--- src/configs/templates.py
+++ src/configs/templates.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi.templating import Jinja2Templates
 
 
-templates = Jinja2Templates(directory="templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "..", "templates"))
