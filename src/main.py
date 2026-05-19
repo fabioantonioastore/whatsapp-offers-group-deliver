aa156f7941b62ffa45eddaaec09d0c4bbd2b5814
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-05-19 17:18:49 -0300

    test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from slowapi.errors import RateLimitExceeded
 
 from .exceptions.rate_limit import rate_limit_exceeded_handler
@@ -6,11 +9,18 @@ from .routers import group_router
 from .configs.environment import DEBUG
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI(
     docs_url=None,
     redoc_url=None,
     debug=False
 )
+app.mount(
+    path="statics",
+    app=StaticFiles(directory=os.path.join(BASE_DIR, "statics")),
+    name="statics"
+)
+
 if DEBUG:
     app = FastAPI(
         debug=True
